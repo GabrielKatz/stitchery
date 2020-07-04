8582b4efa3a44947621322da88e470169a1adf78
Gabriel Katz <gkatz@gmx.net>
2020-07-05 01:07:30 +0800

    Add basic drawing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,51 @@ import React, { useState, ChangeEvent } from 'react';
 import { Input } from 'antd';
 import { CrossStitchEditor } from './editor/cross-stitch-editor';
 import './App.scss';
+import produce from 'immer';
 
 function App() {
   const [height, setHeight] = useState(200);
   const [width, setWidth] = useState(120);
+  const getNewImageForSize = (oldImage: string[][]) => {
+    const newImage: string[][] = [];
+    for(let i=0; i<height; i++){
+      newImage.push([]);
+      for(let j=0; j< width; j++) {
+        newImage[i].push((oldImage[i] && oldImage[i][j]) || '#ffffff');
+      }
+    }
+    return newImage;  
+  }
+  const color = '#000000';
 
-  const onHeightChange = (ev: ChangeEvent<HTMLInputElement>) => setHeight(+ev.target.value);
-  const onWidthChange = (ev: ChangeEvent<HTMLInputElement>) => setWidth(+ev.target.value);
+  const [image, setImage] = useState<string[][]>(getNewImageForSize([]));
+
+  const onHeightChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setHeight(+ev.target.value);
+    setImage(getNewImageForSize(image));
+  }
+  const onWidthChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setWidth(+ev.target.value);
+    setImage(getNewImageForSize(image));
+  }
+
+  const onFieldSelect = (x: number, y: number) => {
+    setImage(produce(image, draft => {
+      draft[y][x]= color;
+    }));
+  }
 
   return (
     <div className="App">
       <Input type="number" addonBefore="height" value={height} onChange={onHeightChange}/>
-      <br/>
+      <br/> 
       <br/>
       <Input type="number" addonBefore="width" value={width} onChange={onWidthChange}/>
       <br/>
       <br/>
-      <CrossStitchEditor height={height} width={width} />
+      <div className="editor-container">
+        <CrossStitchEditor image={image} onSelect={onFieldSelect}/>
+      </div>
     </div>
   );
 }
--- src/editor/cross-stitch-editor.tsx
+++ src/editor/cross-stitch-editor.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Layer, Stage, Line } from "react-konva";
+import { Layer, Stage, Line, Rect } from "react-konva";
+import './cross-stitch-editor.scss';
+import { KonvaEventObject } from "konva/types/Node";
 
 interface CrossStitchEditorProps {
-  height: number;
-  width: number;
+  image: string[][];
+  onSelect: (x: number, y: number) => void;
 }
 
-export const CrossStitchEditor: React.FunctionComponent<CrossStitchEditorProps> = ({ height, width }) => {
+
+export const CrossStitchEditor: React.FunctionComponent<CrossStitchEditorProps> = ({ image, onSelect }) => {
   const ratio = 10;
   const lineFill= "#a0a0a0";
   const verticals : React.ReactNode[] = [];
+  const height = image.length;
+  const width = (image[0] && image[0].length) || 0;
+
   for(let i=0; i <= width; i++) {
     const points = [ i * ratio, 0, i * ratio, height * ratio ];
     verticals.push(
@@ -20,6 +26,15 @@ export const CrossStitchEditor: React.FunctionComponent<CrossStitchEditorProps> 
       </Line>);
   }
   const horizontals : React.ReactNode[] = [];
+
+  const clickHandler = (e: KonvaEventObject<MouseEvent>) => {
+    const stage = e.target.getStage();
+    const pointerPosition = stage!.getPointerPosition();
+    if(!pointerPosition)
+      return;
+    onSelect(Math.floor(pointerPosition.x / ratio), Math.floor(pointerPosition.y / ratio));
+  }
+
   for(let i=0; i <= height; i++) {
     const points = [ 0, i * ratio , width * ratio, i * ratio ];
     horizontals.push(
@@ -29,11 +44,26 @@ export const CrossStitchEditor: React.FunctionComponent<CrossStitchEditorProps> 
             stroke={lineFill}>
       </Line>);
   }
-  return <Stage width={800} height={600} draggable={true}>
+  return <Stage width={width * ratio} height={height * ratio} className="editor" onClick={clickHandler}>
       <Layer>
-        { verticals }
+        { image.map((row, i) => 
+          row.map((column, j) => 
+            column !== '#ffffff' &&
+            <Rect key={i*width + j} 
+                  useCache
+                  x={j * ratio} 
+                  y={i * ratio} 
+                  width={ratio} 
+                  height={ratio} 
+                  strokeWidth={0} 
+                  fill={column}
+                  transformsEnabled="position"
+            />
+          )
+        )}
       </Layer>
       <Layer>
+        { verticals }
         { horizontals }
       </Layer>
     </Stage>;
